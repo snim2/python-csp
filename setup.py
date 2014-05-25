a2387fdf3d0bb6e3e923d78873bec83573c7500e
requires.io <support@requires.io>
2014-05-25 20:48:23 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-import sys
+from setuptools import setup, find_packages
 
 version = '0.1'
-
-if sys.version < '3':
-    package_dir = {'': 'csp'}
-else:
-    package_dir = {'': 'csp3'}
-
 
 setup(name='python-csp',
       version=version,
@@ -17,16 +10,25 @@ setup(name='python-csp',
       long_description="""\
 python-csp adds communicating sequential processes to Python""",
       classifiers=["Intended Audience :: Developers",
-                   ("License :: OSI Approved :: " +
-                    "GNU General Public License (GPL)"),
+                   "License :: OSI Approved :: GNU General Public License (GPL)",
                    "Programming Language :: Python",
                    "Topic :: Software Development :: Libraries",
-                   "Topic :: System :: Distributed Computing"],
+                   "Topic :: System :: Distributed Computing"], 
       keywords='concurrency multicore parallel',
       author='Sarah Mount',
       author_email='s.mount@wlv.ac.uk',
       url='http://github.com/snim2/python-csp/',
       license='GPL',
-      package_dir=package_dir,
-      scripts=['scripts/python-csp', ],
+      packages=find_packages(exclude=['ez_setup', 'examples', 'test',
+                                      'benchmark', 'rst', 'scripts',
+                                  ]),
+      include_package_data=True,
+      zip_safe=True,
+	  scripts=['scripts/python-csp', ],
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
       )
